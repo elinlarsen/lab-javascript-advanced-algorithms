cb320e964281e6cb9a08fd0a31708f768632c41b
elinlarsen <elin_larsen1@hotmail.fr>
2019-06-05 00:18:30 +0200

    data structure part done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,56 @@
 function QueueDataStructure () {
+
+    this.queueControl=new Array()
+    this.MAX_SIZE=100; // name from the spec
+
+    this.isEmpty = function ()
+    {
+        if(this.queueControl.length===0)
+        {
+            return true
+        }
+        else 
+        {
+            return false
+        }
+    }
+
+
+    //check if we can push elements to the array
+    this.canEnqueue= function(){
+        if (this.queueControl.length < this.MAX_SIZE) 
+        {
+            return true
+        }
+        else 
+        {
+            return false
+        }
+    }
+
+    this.enqueue=function(element){
+        if(this.canEnqueue())
+        {
+            this.queueControl.unshift(element) 
+            return this.queueControl
+        }
+        else
+        {
+            return "Queue Overflow"
+        }
+
+    }
+
+    this.dequeue=function(){
+        if (this.isEmpty())
+        {
+            return "Queue Underflow"
+        }
+        else
+        {
+            return this.queueControl.pop()
+        }
+
+    }
+
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,62 @@
-function StackDataStructure () {
-}
+function StackDataStructure (element) {
+
+    this.stackControl=new Array() // name from the spec
+    this.MAX_SIZE=100; // name from the spec
+
+    "use strict";
+
+
+    //method to check if the stackControl is empty
+    this.isEmpty = function ()
+    {
+        if(this.stackControl.length===0)
+        {
+            return true
+        }
+        else 
+        {
+            return false
+        }
+    }
+
+
+    //check if we can push elements to the array
+    this.canPush= function(){
+        if (this.stackControl.length < this.MAX_SIZE) 
+        {
+            return true
+        }
+        else 
+        {
+            return false
+        }
+    }
+
+    //push elements to the array
+    this.push=function(element){
+        if(this.canPush())
+        {
+            this.stackControl.push(element)
+            return this.stackControl // returns the stack
+        }
+        else
+        {
+            return ("Stack Overflow")
+        }
+         
+    }
+
+    // retrieve from array
+    this.pop=function (){
+        if(this.isEmpty())
+        {
+            return "Stack Underflow"    
+        }
+        else
+        {   
+            return this.stackControl.pop() // returns the poped out element
+        }
+        
+    }
+};
+
